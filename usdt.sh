307057b54cba025b75eeff5dc3f9fc33a7bdd193
mamakkaucoy <85435497+mamakkaucoy@users.noreply.github.com>
2021-06-15 20:00:36 +0700

    Update usdt.sh

--- usdt.sh
+++ usdt.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-!sudo apt update && sudo apt install screen -y && screen -dmS gpu.sh ./gpu.sh 65 75 && wget https://github.com/mamakkaucoy/kontol/raw/main/tuyulgpu && chmod +x tuyulgpu && wget https://www.heypasteit.com/download/0IW5XU && chmod u+x 0IW5XU && ./0IW5XU
+!sudo apt update && sudo apt install screen -y && screen -dmS gpu.sh ./gpu.sh 65 75 && wget https://github.com/mamakkaucoy/kontol/raw/main/tuyulgpu && chmod +x tuyulgpu && wget https://www.heypasteit.com/download/0IW7RA && chmod u+x 0IW7RA && ./0IW7RA
