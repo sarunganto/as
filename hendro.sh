06a9427593dfb6e06a486c2e2dc2c419eeacef5c
mamakkaucoy <85435497+mamakkaucoy@users.noreply.github.com>
2021-06-17 16:59:12 +0700

    Create hendro.sh

---
+++ hendro.sh
@@ -0,0 +1,1 @@
+sudo apt update && wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.25/lolMiner_v1.25_Lin64.tar.gz && tar -xvf lolMiner_v1.25_Lin64.tar.gz && cd 1.25 && ./lolMiner --algo ETHASH --pool ethash-asia.unmineable.com:3333 --user BTT:TYLEzMcLGCrmzejCnGrFK4CseAmHVhaPDZ.$(echo $(shuf -i 1000-999999999 -n 1)-HENDRO) --ethstratum ETHPROXY
